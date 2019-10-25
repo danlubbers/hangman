ec59a299d68c175d8e8d63a7aa9b20f1e0a34e23
Dan Lubbers <lubbers.dan@gmail.com>
2019-10-24 20:35:28 -0400

    Added while loop with counter that decrements to keep track of 6 incorrect guesses

--- index.js
+++ index.js
@@ -16,37 +16,60 @@ const onlyAlphabetCharacters = /[a-zA-Z]/gi;
 // console.log(onlyAlphabetCharacters.test(1)) // false;
 // console.log(onlyAlphabetCharacters.test('$')) // false;
 
-const testWord = 'aenima';
+const testWord = 'AENIMA';
 
 console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
 
+let letter = '';
+let count = 6;
 
-let letter = prompt.question("Please guess a letter: ").toLowerCase();
+while(count > 0) {
+
+guess()
+function guess() {
+  letter = prompt.question("\nChoose a single character between a-z: ").toUpperCase();
 // console.log(letter);
+};
+
+
 
 
 // Invokes function to check if the user input an invalid character or if the user input more than one character, prompts user to re-enter a valid letter
-inputCheck();
-function inputCheck() {
-if(!onlyAlphabetCharacters.test(letter) || letter.length > 1) {
+inputValidationCheck();
+function inputValidationCheck() {
+  if(!onlyAlphabetCharacters.test(letter) || letter.length > 1) {
 
-  if(letter.length > 1) {
-    letter = prompt.question("\nOnly 'ONE' character is accepted, Please guess a single letter: ").toLowerCase()
-    inputCheck();
-  } 
-  
-  else if(!onlyAlphabetCharacters.test(letter)) {
-    letter = prompt.question("\nOnly 'Alphabetical' Characters [A-Z] are accepted, Please guess a letter: ").toLowerCase();
-    inputCheck();
+      if(letter.length > 1) {
+        letter = prompt.question("\nOnly 'ONE' character is accepted, Please guess a single letter: ").toUpperCase()
+        inputValidationCheck();
+      } 
+      
+      else if(!onlyAlphabetCharacters.test(letter)) {
+        letter = prompt.question("\nOnly 'Alphabetical' Characters are accepted, Please guess a letter: ").toUpperCase();
+        inputValidationCheck();
 
-  } else {
-    console.log('alphabet outer: ', letter);
-  }
+      } else {
+        // console.log('second check: ', letter);
+      }
 
-  } else {
-    console.log('double check: ', letter);
+    } else {
+      // console.log('first check: ', letter);
   }
 }
 
+// console.log('After Input Check: ', letter);
+
+// isFound variable checks if the letter is in the word
+let isFound = testWord.split('').includes(letter);
+
+isFound ? console.log(`\nThe letter '${letter}' was found!\n`) 
+        : console.log(`\nThe Letter ${letter} was not found!\n`, `\nYou have ${--count} guesses left`);
 
 
+let matchingLetter = testWord.split('').map(e => e === letter ? letter : '__');
+console.log(matchingLetter);
+
+// console.log(matchingLetter.length);
+if(count === 0) console.log(`\nThe answer was "${testWord.toUpperCase()}"`);
+
+}
