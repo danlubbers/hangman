823e955125725f74a6afc8c48d23bdf5dc45092f
Dan Lubbers <lubbers.dan@gmail.com>
2019-10-26 18:34:32 -0400

    refactored the correctCharacters array to be the length of the specified word & map over indexOfAll to push the guessedLetter to each specified index (if multiple occurances are found) into the correctCharacters array

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const wordBank = require("./word-bank.json");
 // Using Regex to check for only upper and lower case letters from A to Z
 const onlyAlphabetCharacters = /[a-zA-Z]/gi;
 
+
+const testWord = 'AENIMA';
+
 // Global Variables / State
 let randomWordIndex = undefined;
 let randomWord = '';
 let guessedLetter = '';
 let count = 6;
-let correctCharacters = [];
+let correctCharacters = Array.apply(null, Array(testWord.length)).map(_=> '__');
 let incorrectCharacters = [];
 let gameResults = {
   "Total Rounds Played": 1,
@@ -17,24 +20,23 @@ let gameResults = {
   "Losses": 0
 };
 
-const testWord = 'AENIMA';
-
-// Greeting when logging in for the First Time
-console.log(`\nWelcome to Hangman!\nPress CTRL-C to stop\n`);
-console.log(`\nWords are chosen at random for each round`);
-console.log(`\nBegin by inputting your first guess below...\n`);
-
 // function generates new random word
 const wordGenerator = () => {
   // Gets a random number between 0 and length of the array and sets it to the variable
   randomWordIndex = Math.floor(Math.random(wordBank) * (wordBank.length - 0) + 0);
   // console.log(randomWordIndex);
-
+  
   // Uses the randomWordIndex to set the actual word from the array to the variable 
   randomWord = wordBank[randomWordIndex].toUpperCase();
-console.log(randomWord);
+  console.log({randomWord});
 }
 wordGenerator();
+
+
+// Greeting when logging in for the First Time
+console.log(`\nWelcome to Hangman!\nPress CTRL-C to stop\n`);
+console.log(`\nWords are chosen at random for each round`);
+console.log(`\nBegin by inputting your first guess below...\n`);
 
 
 // Start of game loop | Game counts down from 6 and once it gets to zero there is either a win or loss and the game restarts. Use Ctrl-C to exit the game and nodejs
@@ -82,25 +84,28 @@ while(count > 0) {
     
     // If guessed letter is found, gets all the indexs of letter in randomWord
     const indexOfAll = testWord.split('').map((letter, idx) => letter === guessedLetter ? idx : null).filter(idx => idx !== null)
-    console.log(indexOfAll);
+    // console.log(indexOfAll);
     
-
-
+    
     if(!correctCharacters.includes(guessedLetter)) {
-      correctCharacters.push(guessedLetter);
-      console.log(correctCharacters);
+      // maps over indexOfAll and pushes the guessedLetter to each index in which it was found
+      indexOfAll.map(indexNum => correctCharacters.splice(indexNum, 1, guessedLetter))
+      // correctCharacters.push(guessedLetter);
+      console.log({correctCharacters});
     }
-
+    
   } else {
     // A wrong guess pushes the letter to the incorrectCharacters array
     incorrectCharacters.push(guessedLetter);
     console.log(`\nThe Letter \x1b[31m'${guessedLetter}' \x1b[37mwas 'NOT' found!\n`); 
-
+    console.log({correctCharacters});
+    
     if(hasCharacterAlreadyBeenGuessed) {
       // Informs the user that they have already guessed that particular letter 
       console.log(`\x1b[33m\nYou have already guessed the letter \x1b[31m\'${guessedLetter}'\x1b[33m\.\nA repeat guess will not count against you but, it also serves no purpose.\nPlease guess a new letter below.\n`);
       // if the letter has not already been guessed and is incorrect the count WILL NOT decrement
       console.log(`\x1b[37m\nYou have \x1b[33m${count} \x1b[37mguesses left\n`);
+      console.log({correctCharacters});
     } else {
       // if the letter has not already been guessed and is incorrect the count will decrement
       console.log(`\nYou have \x1b[33m${--count} \x1b[37mguesses left\n`);
@@ -109,7 +114,7 @@ while(count > 0) {
 
   // if letter matches guessed letter in random word, logs correct guessed letter otherwise logs __ to indicate a blank section that has not been guessed correctly 
   let matchingLetter = testWord.split('').map(letter => letter === guessedLetter ? guessedLetter : '__');
-  console.log(matchingLetter);
+  // console.log({matchingLetter});
 
 
   // Once all the guesses have run out and the user has not been able to solve the answer, we console log the answer here!
@@ -127,7 +132,7 @@ while(count > 0) {
 
     // This resets the 'state' so the game continues to play only until the User Presses Ctrl-C to exit Node
     count = 6;
-    correctCharacters = [];
+    correctCharacters = Array.apply(null, Array(testWord.length)).map(_=> '__');
     incorrectCharacters = []; 
 
     // Increments the Total amount of Rounds played in the gameResults Object
